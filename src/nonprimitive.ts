757b14ed174d7fd9f748f98e28f49dc77260a88c
MTofayelAhmed <tofayelahmed324@gmail.com>
2025-11-09 11:02:59 +0100

    nonprimitive array and tuple discussion

---
+++ src/nonprimitive.ts
@@ -0,0 +1,14 @@
+//  array , object
+
+let  shoppingList: string[] = ["eggs", "milk"]
+
+let shoppingListWithNumber:(string | number)[]=  ["egg", 12, "apple", 5]
+
+//  in array , there is a special type which we called tuple .  
+
+
+//  tuple example 
+
+
+const nameAge:[string, number]= ["ahmed", 25]
+
