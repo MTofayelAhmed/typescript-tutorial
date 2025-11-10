09db2891298141e1e451f22d3404b017a72e3a01
MTofayelAhmed <tofayelahmed324@gmail.com>
2025-11-10 08:40:45 +0100

    done with non-primitive data  object discussion , readonly usecase and declare raw string as a type

--- src/nonprimitive.ts
+++ src/nonprimitive.ts
@@ -12,3 +12,19 @@ let shoppingListWithNumber:(string | number)[]=  ["egg", 12, "apple", 5]
 
 const nameAge:[string, number]= ["ahmed", 25]
 
+// object , readonly , hard-coated string as a type ( noprimitive or reference type discussion)
+
+
+const user : {
+    organization: "Programming Hero";
+//   readonly  organization: string;     
+//   //or we can put it as readonly property where it can only read the value not change the value 
+    firstName: string;
+    middleName?: string;
+    lastName: string;
+}= {
+    organization: "Programming Hero",
+    firstName: "Md",
+    middleName: "Tofayel",
+    lastName: "Ahmed"
+}
