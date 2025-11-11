c711c08f01aa86ba0b6804db14200c4ea050dce9
MTofayelAhmed <tofayelahmed324@gmail.com>
2025-11-12 00:10:19 +0100

    done with interface

--- src/interface.ts
+++ src/interface.ts
@@ -13,4 +13,33 @@ type Role = {
   role: "admin" | "user";
 };
 
+
+interface IUser {
+    name: string;
+    age: number 
+}
+
+
+interface UserWithRole extends IUser{
+    role: "admin",
+}
+
 type userWithRole = User & Role;
+
+// function and array interface discussion 
+
+
+type Add = (num1:number, num2: number)=>   number
+
+interface IAdd {
+    (num1: number, num2: number): number
+}
+
+type friends = string[]
+
+
+interface IFriend{
+    [index:number]: string
+}
+
+const friends : IFriend =["A", "B", "C"]
