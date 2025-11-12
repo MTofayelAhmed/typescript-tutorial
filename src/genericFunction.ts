8ce6c82db772aa2485ea8b423168d5004ba9725d
MTofayelAhmed <tofayelahmed324@gmail.com>
2025-11-13 00:03:25 +0100

    done with tuple generic

--- src/genericFunction.ts
+++ src/genericFunction.ts
@@ -1,18 +1,36 @@
-const createArrayWithString  = (value: string)=> {
-    return [value]
-}
-const createArrayWithNumber  = (value:number )=> {
-    return [value]
-}
-const createArrayWithObj  = (value: {id: number, name: string})=> {
-    return [value]
-}
+// const createArrayWithString  = (value: string)=> {
+//     return [value]
+// }
+// const createArrayWithNumber  = (value:number )=> {
+//     return [value]
+// }
+// const createArrayWithObj  = (value: {id: number, name: string})=> {
+//     return [value]
+// }
 
-const arrString = createArrayWithString("apple")
-const arrNumb = createArrayWithNumber(100)
-const arrObj = createArrayWithObj({id:222, name: "ajfdksajf"})
+
 
 
 const createArrayWithGeneric = <T>(value: T)=> {
 return [value]
 }
+
+const arrString = createArrayWithGeneric("apple")
+const arrNumb = createArrayWithGeneric(100)
+const arrObj = createArrayWithGeneric({id:222, name: "ajfdksajf"})
+
+
+// tuple 
+const createArrayWithTuple = (param1: string, param2: number)=> {
+    return [param1, param2]
+}
+
+
+const tupleArr = createArrayWithTuple("ahmed", 222)
+
+
+const createArrayTupleWithGeneric =<X, Y> (param1: X, param2: Y)=> {
+    return [param1, param2]
+}
+
+const res1 = createArrayTupleWithGeneric("stong", true)
