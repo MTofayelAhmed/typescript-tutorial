6800ede42edcf904f813e2d7b00cc73b657cd972
MTofayelAhmed <tofayelahmed324@gmail.com>
2025-11-12 01:23:01 +0100

    done with generic

---
+++ src/generic.ts
@@ -0,0 +1,12 @@
+type GenericArray<T> = Array<T>;
+
+const friends: GenericArray<string> = ["A", "B", "C"];
+
+const rollNumber: GenericArray<number> = [1, 2, 3, 4];
+
+const isEligible: GenericArray<boolean> = [true, false, true];
+
+type Coordinates<T, Y> = [T, Y];
+
+const coordinates1: Coordinates<string, string> = ["20", "30"];
+const coordinates2: Coordinates<number, number> = [20, 30];
