7dd3f7432da33e6f85d83aa670240a8e71234c0f
MTofayelAhmed <tofayelahmed324@gmail.com>
2025-11-14 12:18:45 +0100

    done with as const discussion

---
+++ src/asConst.ts
@@ -0,0 +1,29 @@
+// As constance 
+
+
+ // set of fixed string leteral ek jaigai rakhe
+
+// type UserRoles = "Admin" | "Editor" | "Viewer";
+
+// enum UserRoles {
+//   Admin = "Admin",
+//   Editor = "Editor",
+//   Viewer = "Viewer",
+// }
+
+
+const UserRoles = {
+    Admin : "Admin",
+  Editor : "Editor",
+  Viewer : "Viewer",
+} as const 
+
+
+const canEdit = (role: keyof typeof UserRoles) => {
+  if (role === UserRoles.Admin || role === UserRoles.Editor) {
+    return true;
+  } else return false;
+};
+
+const isEditPermissable = canEdit(UserRoles.Admin);
+console.log(isEditPermissable);
