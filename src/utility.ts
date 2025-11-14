3fa90337c77f61f618da78c274055154cb1735d7
MTofayelAhmed <tofayelahmed324@gmail.com>
2025-11-14 14:44:32 +0100

    done with required and optional type

--- src/utility.ts
+++ src/utility.ts
@@ -11,6 +11,13 @@ type ProductSummary= Pick<Product, "id"| "name"|"price">
 
 type ProductWithoutStock = Omit<Product, "stock">
 
+// if we want to make every things should be required 
+
+
+type ProductWithEverything = Required<Product>
+
+type OptionalProduct = Partial <Product>
 
 
 
+ 
