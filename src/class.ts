93ad45d2f6942e6173ee509d8f5d965e8103de87
MTofayelAhmed <tofayelahmed324@gmail.com>
2025-11-15 10:03:40 +0100

    done with class method

--- src/class.ts
+++ src/class.ts
@@ -1,14 +1,20 @@
 class Animal {
-    name: string;
-    species: string; 
-    sound: string ;
+  name: string;
+  species: string;
+  sound: string;
 
-    constructor(name: string, species: string, sound: string ){
-        this.name = name 
-        this.species = species 
-        this.sound = sound 
-    }
+  constructor(name: string, species: string, sound: string) {
+    this.name = name;
+    this.species = species;
+    this.sound = sound;
+  }
+
+  makeSound() {
+    console.log(`${this.name}is making sound ${this.sound}   `);
+  }
 }
-const dog = new Animal ("DOg", "doggi", "ghew ghew")
+const dog = new Animal("DOg", "doggi", "ghew ghew");
 
-console.log(dog.name)
+dog.makeSound();
+
+console.log(dog.name);
