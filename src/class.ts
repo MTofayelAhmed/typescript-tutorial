b26a62b04ea7cd0332fce7ea3377be8c3ecc5ee0
MTofayelAhmed <tofayelahmed324@gmail.com>
2025-11-15 10:06:46 +0100

    done with parameter properties

--- src/class.ts
+++ src/class.ts
@@ -1,13 +1,31 @@
-class Animal {
-  name: string;
-  species: string;
-  sound: string;
+// class Animal {
+//   name: string;
+//   species: string;
+//   sound: string;
 
-  constructor(name: string, species: string, sound: string) {
-    this.name = name;
-    this.species = species;
-    this.sound = sound;
-  }
+//   constructor(name: string, species: string, sound: string) {
+//     this.name = name;
+//     this.species = species;
+//     this.sound = sound;
+//   }
+
+//   makeSound() {
+//     console.log(`${this.name}is making sound ${this.sound}   `);
+//   }
+// }
+// const dog = new Animal("DOg", "doggi", "ghew ghew");
+
+// dog.makeSound();
+
+// console.log(dog.name);
+
+// parameter properties
+class Animal {
+  constructor(
+    public name: string,
+    public species: string,
+    public sound: string
+  ) {}
 
   makeSound() {
     console.log(`${this.name}is making sound ${this.sound}   `);
