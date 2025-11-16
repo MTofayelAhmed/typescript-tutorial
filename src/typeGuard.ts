481808b3c62e4b57bc4cee420ca64d0cd141fb12
MTofayelAhmed <tofayelahmed324@gmail.com>
2025-11-16 11:05:24 +0100

    done with type guard

---
+++ src/typeGuard.ts
@@ -0,0 +1,56 @@
+// type guard
+
+// in typeof
+
+// type Alphaneumeric = number | string;
+
+// const add = (num1: Alphaneumeric, num2: Alphaneumeric) => {
+//   if (typeof num1 === "number" && typeof num2 === "number") {
+//     return num1 + num2;
+//   } else {
+//     num1.toString() + num2.toString();
+//   }
+// };
+
+// add(2, 2); // 4
+
+// add(2, "2"); // 22
+
+// add("2", 2); // 22
+
+// add("2", "2"); //
+
+const add = (num1:number | string, num2: number | string)=> {
+    if (typeof num1=== "number" && typeof num2 === "number")
+        {
+
+return num1 + num2 
+
+        }
+
+        else {
+            return num1.toString() + num2.toString
+        }
+
+}
+
+// in guard
+
+type NormalUser = {
+  name: string;
+};
+
+type AdminUser = {
+  name: string;
+  role: "Admin";
+};
+
+const getUserInfo = (user: NormalUser | AdminUser) => {
+  if ("role" in user) {
+    console.log(`${user.name} and his rolwe is : ${user.role}`);
+  } else {
+    console.log(` ${user.name}`);
+  }
+};
+
+getUserInfo({ name: "Normal", role: "Admin" });
