2674f5ad76b1e79d5df3fd615c1cd0e9de16e026
MTofayelAhmed <tofayelahmed324@gmail.com>
2025-11-16 23:34:57 +0100

    done with polymorphism

--- src/polymorphism.ts
+++ src/polymorphism.ts
@@ -16,7 +16,7 @@ class NextLevelDeveloper extends Person{
     }
 }
 const getSleepingHour = (param: Person)=> {
-  
+  param.getSleep( )
 }
 
 
@@ -24,3 +24,50 @@ const person1 = new Person()
 const person2 = new Student()
 const person3 = new NextLevelDeveloper()
 getSleepingHour(person1)
+
+class Shape {
+  getArea(): number {
+    return 0;
+  }
+}
+
+class Circle extends Shape {
+  // area= pi*r*r
+  radius: number;
+
+  constructor(radius: number) {
+    super();
+    this.radius = radius;
+  }
+
+  getArea(): number {
+    return Math.PI * this.radius * this.radius;
+  }
+}
+
+class Rectangle extends Shape {
+  // area= height*width
+
+  height: number;
+  width: number;
+
+  constructor(height: number, width: number) {
+    super();
+    this.height = height;
+    this.width = width;
+  }
+
+  getArea(): number {
+    return this.height * this.width;
+  }
+}
+
+const getArea = (param: Shape) => {
+  console.log(param.getArea());
+};
+
+const shape1 = new Shape();
+const shape2 = new Circle(10);
+const shape3 = new Rectangle(10, 20);
+
+getArea(shape3);
